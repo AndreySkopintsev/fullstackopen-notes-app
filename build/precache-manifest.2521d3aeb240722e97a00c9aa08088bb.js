dac5b86ede7fcdc3a9cfb83bee2d3221a7f66328
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2020-06-02 13:54:08 +0300

    adding front end

---
+++ build/precache-manifest.2521d3aeb240722e97a00c9aa08088bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a0a302bc3a89e15a6b397389a3ed17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1129c05de73b3238b688",
+    "url": "/static/css/main.f6287125.chunk.css"
+  },
+  {
+    "revision": "c010a903ea0d71584dd1",
+    "url": "/static/js/2.cde86dd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cde86dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1129c05de73b3238b688",
+    "url": "/static/js/main.5c859a2c.chunk.js"
+  },
+  {
+    "revision": "f7f8a1afa063bcf92618",
+    "url": "/static/js/runtime-main.08dc2b48.js"
+  }
+]);
